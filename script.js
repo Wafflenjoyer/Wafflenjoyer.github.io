ca518dc52874717e44b8f1003d33ed374de9d483
Wafflenjoyer <132163248+Wafflenjoyer@users.noreply.github.com>
2024-10-24 20:41:10 -0700

    made changes to the login page to make it more appealing but still not functional

--- script.js
+++ script.js
@@ -1,19 +1,4 @@
-/* const banner = document.querySelector('.interactive-banner');
 
-banner.addEventListener('click', () => {
-  // Add some animation or effect here
-  console.log('Banner clicked!');
-});
-
-banner.addEventListener('mouseover', () => {
-  // Add some animation or effect here
-  console.log('Banner hovered!');
-});
-
-banner.addEventListener('mouseout', () => {
-  // Add some animation or effect here
-  console.log('Banner mouseout!');
-}); */
 
 function generateCalendar() {
   const calendarBody = document.getElementById('calendar-body');
